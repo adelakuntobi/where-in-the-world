cd57db5588348c409e75a23af4c1b61e8d2824d4
adelakuntobi <adelakuntobiloba1@gmail.com>
2020-09-17 01:39:19 +0100

    Fam, we areb't live yet. Just added search bar

--- src/components/FetchData/FetchData.js
+++ src/components/FetchData/FetchData.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 import Eachcountry from '../Eachcountry/Eachcountry'
+import Filter from '../Filter/Filter';
+import Search from '../Search/Search';
 
 function FetchData() {
   const [countries, setCountries] = useState([])
+  const [region, setRegion] = useState('');
+
+  const handleSelect = (event) => {
+    setRegion(event.target.value);
+  };
   useEffect(() => {
     Axios.get('https://restcountries.eu/rest/v2/all')
       .then(res => {
@@ -13,13 +20,30 @@ function FetchData() {
         console.log(err)
       })
   }, [])
+
+  function handleSearch(searchresult){
+    // var searchresult = new []();
+    // console.log(searchresult)
+    setCountries(searchresult)
+  }
   return (
-    <div 
-    className="my-12 px-4 md:px-0 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 container">
-      {
-        countries.map(country =>
-          <Eachcountry key={country.name} country={country} />)
-      }
+
+    <div>
+      <div className="flex flex-row w-full justify-between">
+        <Search country={countries} handleSearch={handleSearch}/>
+        <Filter
+          region={region}
+          handleSelect={handleSelect}
+        />
+      </div>
+      <div
+        className="my-12 px-4 md:px-0 grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 container">
+        {
+          countries.map(country =>
+            // console.log(country.name),
+            <Eachcountry key={country.name} country={country} />)
+        }
+      </div>
     </div>
   )
 }
